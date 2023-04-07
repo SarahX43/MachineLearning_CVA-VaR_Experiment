6e3e04e2f0603a695915f3814eb99c9aeb6a2b11
SarahX43 <sarah.xie@mail.utoronto.ca>
2023-04-07 17:16:54 -0400

    Data Cleaning

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 import torch
 import pandas as pd
 
-df = pd.read_csv("Stock Prices.csv")
-df.head()
+df = pd.read_csv('Stock Prices.csv')
+print(df.shape)
+print(df.head(10))
+
+df2 = df[df.columns[1:]]
+print(df2.shape)
+print(df2.head(10))
+
+df3 = df2.iloc[1:]
+print(df3.shape)
+print(df3.head(10))
